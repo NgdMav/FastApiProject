b0398918e12a35b88d9971478d0d2bfd4ffd2b62
Mav <riherr00@gmail.com>
2025-12-10 21:11:14 +0300

    Validation with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -12,6 +21,6 @@ def get_posts():
     return {"data":"Your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post":f"title: {payload['title']}, content: {payload['content']}"}
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data": new_post.model_dump()}
