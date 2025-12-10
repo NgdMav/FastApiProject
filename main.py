67200a963bf0f81edf7b0b229157bc70c5619e4a
Mav <riherr00@gmail.com>
2025-12-10 21:38:46 +0300

    Get one post by id

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randrange
 from typing import Optional
 
 from fastapi import FastAPI
@@ -12,15 +13,31 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "Title of post 1", "content": "Content of post 1", "id": 1},
+            {"title": "Title of post 2", "content": "Content of post 2", "id": 2},]
+
+def find_post(id: int) -> dict:
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    return dict()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"Your posts"}
+    return {"data":my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post)
-    return {"data": new_post.model_dump()}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"data": post}
