774316917c87eac306fb7fa1a3ef4bbcb8ede5e9
Mav <riherr00@gmail.com>
2025-12-14 09:53:24 +0300

    Clean up. Deleting and updating only your posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -8,19 +8,13 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.get("/", response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM fastapi.fastapi.posts;")
-    # posts = cursor.fetchall()
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     posts = db.query(models.Post).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
                  current_user: schemas.CurrentUser = Depends(oauth2.get_current_user)):
-    # cursor.execute("""INSERT INTO fastapi.fastapi.posts (title, content, published)
-    #                   VALUES (%s, %s, %s) returning *;""",
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
     new_post = models.Post(**post.model_dump(), owner_id=current_user.id)
     db.add(new_post)
     db.commit()
@@ -28,46 +22,40 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
     return new_post
 
 @router.get("/{post_id}", response_model=schemas.PostResponse)
-def get_post(post_id: int, db: Session = Depends(get_db),
-             current_user: str = Depends(oauth2.get_current_user)):
-    # cursor.execute("""SELECT * FROM fastapi.fastapi.posts WHERE id = %s;""",
-    #                (str(post_id),))
-    # post = cursor.fetchone()
+def get_post(post_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
-
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+    #                         detail="You do not have permission to perform this action")
     return post
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int, db: Session = Depends(get_db),
-                current_user: str = Depends(oauth2.get_current_user)):
-    # cursor.execute("""DELETE FROM fastapi.fastapi.posts WHERE id = %s returning *;""",
-    #                (str(post_id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
+                current_user: schemas.CurrentUser = Depends(oauth2.get_current_user)):
     deleted_post = db.query(models.Post).filter(models.Post.id == post_id)
     if deleted_post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
+    if deleted_post.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You do not have permission to perform this action")
     deleted_post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{post_id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostResponse)
 def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db),
-                current_user: str = Depends(oauth2.get_current_user)):
-    # cursor.execute("""UPDATE fastapi.fastapi.posts SET title = %s, content = %s,
-    #                                                    published = %s where id = %s
-    #                   returning *;""",
-    #                (post.title, post.content, post.published, post_id))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
+                current_user: schemas.CurrentUser = Depends(oauth2.get_current_user)):
     updated_post = db.query(models.Post).filter(models.Post.id == post_id)
     if updated_post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
+    if updated_post.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You do not have permission to perform this action")
     updated_post.update(post.model_dump(), synchronize_session=False)
     db.commit()
     return updated_post.first()
